16cd4d14415ebb0181d1250293155601c48928ee
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-01 01:11:03 -0300

    Atualização

--- assets/js/scroll.js
+++ assets/js/scroll.js
@@ -8,7 +8,7 @@ html.addEventListener('mouseout', msg2=()=> {
     html.src='assets/image/html-verde.svg';
 })
 
-const css = document.getElementById("logo-html-css");
+const css = document.getElementById("logo-css-hover");
 
 css.addEventListener('mouseover', msg=()=> {
     css.src='assets/image/css-cor.svg';
@@ -16,4 +16,14 @@ css.addEventListener('mouseover', msg=()=> {
 
 css.addEventListener('mouseout', msg2=()=> {
     css.src='assets/image/css-verde.svg';
+})
+
+const js = document.getElementById("logo-js-hover");
+
+js.addEventListener('mouseover', msg=()=> {
+    js.src='assets/image/js-cor.svg';
+})
+
+js.addEventListener('mouseout', msg2=()=> {
+    js.src='assets/image/js-verde.svg';
 })
