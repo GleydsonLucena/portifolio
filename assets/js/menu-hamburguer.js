5cf238a3f2794f5b10d866f44704727cd2d9b26e
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-03-03 04:00:20 -0300

    Update menu-hamburguer.js

--- assets/js/menu-hamburguer.js
+++ assets/js/menu-hamburguer.js
@@ -1,6 +1,5 @@
 const navbar = document.getElementById('navbar-mobile-ul');
 const menu = document.getElementById('menu-hamburguer');
-const sec = document.getElementsByTagName('div')
 
 menu.addEventListener('click', openMenu=()=> {
     if(navbar.style.display == 'none') {
@@ -9,10 +8,3 @@ menu.addEventListener('click', openMenu=()=> {
         navbar.style.display = 'none'
     };
 })
-
-sec.addEventListener('click' clicou=()=> {
-    alert('opa')
-})
-
-// else if (navbar.style.display == 'flex') {
-//     navbar.style.display = 'none'
