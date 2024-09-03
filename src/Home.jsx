d0618395fcdcbbf594ecbcfb19e21573cd9eb670
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-09-03 15:04:19 -0300

    feat: created container

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,30 @@
-import { makeStyles } from "@mui/styles";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import CssBaseline from "@mui/material/CssBaseline";
+import { Container } from "@mui/material";
 
-const Home = () => {
-  const classes = useStyles();
+const darkTheme = createTheme({
+  palette: {
+    mode: "dark",
+    primary: {
+      main: "#67159C",
+    },
+    secondary: {
+      main: "#07D962",
+    },
+    bg: {
+      main: "#1E1E26",
+      dark: "#19181F",
+    },
+  },
+});
+
+export default function App() {
   return (
-    <div className={classes.root}>
-      <h1>Hello World</h1>
-    </div>
+    <ThemeProvider theme={darkTheme}>
+      <CssBaseline />
+      <Container maxWidth="lg" sx={{ mt: 4, mb: 4 }}>
+        <h1>APP</h1>
+      </Container>
+    </ThemeProvider>
   );
-};
-
-export default Home;
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,9 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { ThemeProvider, createTheme } from "@mui/material/styles";
 import Home from "./Home";
-
-const theme = createTheme({
-  palette: {
-    primary: {
-      main: "#67159C",
-    },
-    secondary: {
-      main: "#07D962",
-    },
-    bg: {
-      main: "#1E1E26",
-      dark: "#19181F",
-    },
-  },
-});
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <ThemeProvider theme={theme}>
-      <Home />
-    </ThemeProvider>
+    <Home />
   </StrictMode>
 );
