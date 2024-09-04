9e9314e8b3b4abd3bab8efd0b612e51c03ad48a8
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-09-03 21:09:24 -0300

    feat: created component de Navabr

---
+++ src/sections/Hero.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const StyledHero = styled.div`
+  min-height: 100vh;
+  background-color: red;
+`;
+
+const Hero = () => {
+  return <StyledHero>Hero</StyledHero>;
+};
+
+export default Hero;
