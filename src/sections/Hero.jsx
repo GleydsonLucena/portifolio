7029797c245d21f1a8ee69d871f2b31435f4190b
Gleydson Lucena <gleydsonlucena3@gmail.com>
2024-09-09 18:08:49 -0300

    WIP .

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -146,7 +146,7 @@ const Hero = () => {
               mb: 5,
             }}
           >
-            Gleydson Lucena<span>.</span>
+            Gleydson Lucena
           </Typography>
           <Typography
             variant="subtitle2"
